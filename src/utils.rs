eb7a94af0ba5a93bc3835ba7c8b1c2093b91b288
Jose Victor <josevictorferreira@outlook.com>
2023-12-02 17:16:34 -0300

    fixing formatting

--- src/utils.rs
+++ src/utils.rs
@@ -1,7 +1,6 @@
 #![allow(dead_code)]
 pub fn read_input(input_day_name: &str) -> String {
     let filepath = format!("./input/{}.txt", input_day_name);
-    let file_contents =
-    std::fs::read_to_string(filepath).expect("Unable to read file");
+    let file_contents = std::fs::read_to_string(filepath).expect("Unable to read file");
     return file_contents;
 }
