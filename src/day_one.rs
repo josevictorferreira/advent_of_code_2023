22a5669d26a819a13d13f7189a1390026efda9b1
Jose Victor <josevictorferreira@outlook.com>
2023-12-03 01:16:49 -0300

    finally finished day one

--- src/day_one.rs
+++ src/day_one.rs
@@ -1,42 +1,46 @@
 #[path = "./utils.rs"]
 mod utils;
-use utils::read_input;
+
+use utils::DAY_ONE;
+use utils::DIGITS_PATTERN;
+use utils::SPELLED_NUMBERS_PATTERN;
+use utils::REVERSE_SPELLED_NUMBERS_PATTERN;
 
 pub fn main() {
-    let input_file_contents = read_input("day_one");
-    let sum = sum_calibration_numbers(input_file_contents);
-    println!("sum: {}", sum);
+    let input = utils::read_input(DAY_ONE);
+    let sum_part_one = sum_calibration_digits(input.clone());
+    let sum_part_two = sum_calibration_spelled_numbers(input);
+    println!("part one: {}", sum_part_one);
+    println!("part two: {}", sum_part_two);
 }
 
-fn get_text_first_digit(input: &str) -> i32 {
-    for char in input.chars() {
-        if char.is_digit(10) {
-            return char.to_string().as_str().parse().unwrap();
-        }
-    }
-    panic!("No digit found in input: {}", input);
-}
-
-fn get_text_last_digit(input: &str) -> i32 {
-    for char in input.chars().rev() {
-        if char.is_digit(10) {
-            return char.to_string().as_str().parse().unwrap();
-        }
-    }
-    panic!("No digit found in input: {}", input);
-}
-
-fn get_calibration_number_from_line(input: &str) -> i32 {
-    let first_digit: i32 = get_text_first_digit(input);
-    let last_digit: i32 = get_text_last_digit(input);
+fn get_calibration_digit_from_line(input: &str) -> i32 {
+    let first_digit: i32 = utils::get_text_first_ocurrence(input, DIGITS_PATTERN);
+    let last_digit: i32 = utils::get_text_last_ocurrence(input, DIGITS_PATTERN);
     let calibration_digit: i32 = format!("{}{}", first_digit, last_digit).parse().unwrap();
     return calibration_digit;
 }
 
-fn sum_calibration_numbers(text: String) -> i32 {
+fn get_calibration_spelled_number_from_line(input: &str) -> i32 {
+    let first_digit: i32 = utils::get_text_first_ocurrence(input, SPELLED_NUMBERS_PATTERN);
+    let last_digit: i32 = utils::get_text_last_ocurrence(input, REVERSE_SPELLED_NUMBERS_PATTERN);
+    let calibration_digit: i32 = format!("{}{}", first_digit, last_digit).parse().unwrap();
+    return calibration_digit;
+}
+
+fn sum_calibration_digits(text: String) -> i32 {
     let mut sum: i32 = 0;
     for line in text.lines() {
-        let calibration_number = get_calibration_number_from_line(line);
+        let calibration_number = get_calibration_digit_from_line(line);
+        sum += calibration_number;
+    }
+    return sum;
+}
+
+fn sum_calibration_spelled_numbers(text: String) -> i32 {
+    let mut sum: i32 = 0;
+    for line in text.lines() {
+        let calibration_number = get_calibration_spelled_number_from_line(line);
         sum += calibration_number;
     }
     return sum;
@@ -47,45 +51,44 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_get_first_digit() {
-        assert_eq!(get_text_first_digit("1abc2"), 1);
-    }
-    #[test]
-    #[should_panic]
-    fn test_get_first_digit_panic() {
-        get_text_first_digit("abc");
+    fn test_get_calibration_digit_from_line() {
+        assert_eq!(get_calibration_digit_from_line("1abc2"), 12);
     }
 
     #[test]
-    #[should_panic]
-    fn test_get_last_digit_panic() {
-        get_text_last_digit("abc");
+    fn test_get_calibration_digit_from_line_2() {
+        assert_eq!(get_calibration_digit_from_line("treb7uchet"), 77);
     }
 
     #[test]
-    #[should_panic]
-    fn test_get_calibration_number_from_line_panic() {
-        get_calibration_number_from_line("abc");
+    fn test_get_calibration_spelled_number_from_line() {
+        assert_eq!(get_calibration_spelled_number_from_line("two1nine"), 29);
     }
 
     #[test]
-    fn test_get_last_digit() {
-        assert_eq!(get_text_last_digit("1abc2"), 2);
+    fn test_get_calibration_spelled_number_from_line_2() {
+        assert_eq!(get_calibration_spelled_number_from_line("zoneight234"), 14);
     }
 
     #[test]
-    fn test_get_calibration_number_from_line() {
-        assert_eq!(get_calibration_number_from_line("1abc2"), 12);
+    fn test_get_calibration_spelled_number_from_line_3() {
+        assert_eq!(get_calibration_spelled_number_from_line("eighthree"), 83);
     }
 
     #[test]
-    fn test_get_calibration_number_from_line_2() {
-        assert_eq!(get_calibration_number_from_line("treb7uchet"), 77);
+    fn test_get_calibration_spelled_number_from_line_4() {
+        assert_eq!(get_calibration_spelled_number_from_line("sevenine"), 79);
     }
 
     #[test]
-    fn test_sum_calibration_numbers() {
-        let test_file_contents = read_input("day_one_test");
-        assert_eq!(sum_calibration_numbers(test_file_contents), 142);
+    fn test_sum_calibration_digits() {
+        let test_file_contents = utils::read_test_input(DAY_ONE, utils::PART_ONE);
+        assert_eq!(sum_calibration_digits(test_file_contents), 142);
+    }
+
+    #[test]
+    fn test_sum_calibration_spelled_numbers() {
+        let test_file_contents = utils::read_test_input(DAY_ONE, utils::PART_TWO);
+        assert_eq!(sum_calibration_spelled_numbers(test_file_contents), 281);
     }
 }
--- src/utils.rs
+++ src/utils.rs
@@ -1,6 +1,112 @@
-#![allow(dead_code)]
-pub fn read_input(input_day_name: &str) -> String {
-    let filepath = format!("./input/{}.txt", input_day_name);
+use regex::Regex;
+use std::collections::HashMap;
+
+pub const DAY_ONE: i32 = 1;
+
+#[allow(dead_code)]
+pub const PART_ONE: i32 = 1;
+#[allow(dead_code)]
+pub const PART_TWO: i32 = 2;
+
+pub const DIGITS_PATTERN: &str = r"1|2|3|4|5|6|7|8|9";
+pub const SPELLED_NUMBERS_PATTERN: &str = r"one|two|three|four|five|six|seven|eight|nine|1|2|3|4|5|6|7|8|9";
+pub const REVERSE_SPELLED_NUMBERS_PATTERN: &str = r"eno|owt|eerht|ruof|evif|xis|neves|thgie|enin|1|2|3|4|5|6|7|8|9";
+
+fn get_days_text() -> HashMap<i32, &'static str> {
+    return HashMap::from([
+        (1, "one"),
+        (2, "two"),
+        (3, "three"),
+        (4, "four"),
+        (5, "five"),
+        (6, "six"),
+        (7, "seven"),
+        (8, "eight"),
+        (9, "nine"),
+        (10, "ten"),
+        (11, "eleven"),
+        (12, "twelve"),
+        (13, "thirteen"),
+        (14, "fourteen"),
+        (15, "fifteen"),
+        (16, "sixteen"),
+        (17, "seventeen"),
+        (18, "eighteen"),
+        (19, "nineteen"),
+        (20, "twenty"),
+        (21, "twenty_one"),
+        (22, "twenty_two"),
+        (23, "twenty_three"),
+        (24, "twenty_four"),
+        (25, "twenty_five"),
+    ]);
+}
+
+fn word_to_number(word: &str) -> Option<i32> {
+    match word {
+        "one" | "1" => Some(1),
+        "two" | "2" => Some(2),
+        "three" | "3" => Some(3),
+        "four" | "4" => Some(4),
+        "five" | "5" => Some(5),
+        "six" | "6" => Some(6),
+        "seven" | "7" => Some(7),
+        "eight" | "8" => Some(8),
+        "nine" | "9" => Some(9),
+        _ => None,
+    }
+}
+
+fn word_reverse_to_number(word: &str) -> Option<i32> {
+    match word {
+        "eno" | "1" => Some(1),
+        "owt" | "2" => Some(2),
+        "eerht" | "3" => Some(3),
+        "ruof" | "4" => Some(4),
+        "evif" | "5" => Some(5),
+        "xis" | "6" => Some(6),
+        "neves" | "7" => Some(7),
+        "thgie" | "8" => Some(8),
+        "enin" | "9" => Some(9),
+        _ => None,
+    }
+}
+
+pub fn read_input(day_num: i32) -> String {
+    let days_text: HashMap<i32, &str> = get_days_text();
+    let input_day_name = days_text.get(&day_num).unwrap();
+    let filepath = format!("./input/day_{}/input.txt", input_day_name);
+    return read_file(&filepath);
+}
+
+#[allow(dead_code)]
+pub fn read_test_input(day_num: i32, part_num: i32) -> String {
+    let days_text: HashMap<i32, &str> = get_days_text();
+    let input_day_name = days_text.get(&day_num).unwrap();
+    let filepath = format!("./input/day_{}/part_{}_test.txt", input_day_name, part_num);
+    return read_file(&filepath);
+}
+
+fn read_file(filepath: &str) -> String {
     let file_contents = std::fs::read_to_string(filepath).expect("Unable to read file");
     return file_contents;
+}
+
+pub fn get_text_first_ocurrence(input: &str, pattern: &str) -> i32 {
+    let re = Regex::new(&pattern).unwrap();
+    let first_match = re.find(input).unwrap().as_str();
+    return match word_to_number(first_match) {
+        Some(number) => number,
+        None => panic!("Could not convert {} to number", first_match),
+    };
+}
+
+pub fn get_text_last_ocurrence(input: &str, reverse_pattern: &str) -> i32 {
+    let reverse_input = input.chars().rev().collect::<String>();
+    let re = Regex::new(&reverse_pattern).unwrap();
+    let first_match = re.find(&reverse_input).unwrap().as_str();
+    return match word_reverse_to_number(first_match) {
+        Some(number) => number,
+        None => panic!("Could not convert {} to number", first_match),
+    };
 }
